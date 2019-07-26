5e903d327ebf836e6618ad913707a491cea1a658
jfboy <2414528430@qq.com>
2019-07-26 18:24:28 +0800

    first commit

---
+++ Photo_Spider/bimage.py
@@ -0,0 +1,67 @@
+﻿from _md5 import md5  #可以在后面保存图片的时候起到去重的作用
+from gevent import os
+from requests import RequestException
+import re
+import requests
+import photo_wall
+
+def get_page(keyword): #获取网页
+    url ='https://image.baidu.com/search/index?tn=baiduimage&word='+ keyword
+    try:
+        re = requests.get(url)
+        if re.status_code == 200:
+            html = re.text
+            return html
+        return None
+    except RequestException:
+        print("获取网页出错！")
+        return None
+
+def parse_page(html,new_path ): #分析网页
+    pattern = re.compile('"objURL":"(.*?)"')  #data-objurl="(.*?)"
+    result = re.findall(pattern,html)
+    for url in result:
+        download_images(url,new_path )
+
+def download_images(url,new_path ): #下载图片
+    print("正在下载", url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            save_images(response.content,new_path ) #保存图片
+            print("下载成功！")
+        return None
+    except RequestException:
+        print("下载图片出错",url)
+        return None
+
+def save_images(content,new_path ): #保存图片
+    file_path = '{0}/{1}.{2}'.format(new_path, md5(content).hexdigest(), 'jpg')  # 获得当前项目路径并保存,md5判断，可去重
+    if not os.path.exists(file_path):  # 若文件不存在，则存储下来
+        with open(file_path, 'wb') as f:  # 写入二进制
+            f.write(content)
+            f.close()
+
+def new_dir(path_name):
+    new_path = os.getcwd() + os.path.sep + path_name  # 新建的文件夹的路径
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+    else:
+        print("文件夹已存在！")
+    return new_path
+
+def main(keyword):
+    new_path = new_dir(keyword)
+    html = get_page(keyword)
+    parse_page(html, new_path)
+    photo_wall.main(keyword)
+
+if __name__ == "__main__":
+    with open('keyword.txt', 'rt') as f:
+        keyword = f.readline()
+        f.close()
+    if keyword:
+        print('关键字:', keyword)
+        main(keyword)
+    else:print('关键字不能为空')
+
---
+++ Photo_Spider/photo_wall.py
@@ -0,0 +1,82 @@
+﻿from PIL import Image
+import os
+import glob
+import math
+
+#连续获取文件夹的图片，并制成照片墙
+def get_images(images,source_path,keyword,WALL_SIZE):
+    imgs_path = source_path + '\\*.jpg'
+    img_number= len(glob.glob(imgs_path)) #图片的总个数
+    NEW_SIZE = get_oneimg_size(WALL_SIZE, img_number)#计算墙内每张图片的size
+    row , column = get_row_column(WALL_SIZE,NEW_SIZE)#计算行列个数
+    x_offset, y_offset = get_offset(WALL_SIZE, NEW_SIZE, row, column) #计算行列间隔值
+    for img_path in glob.glob(imgs_path):
+        img_resize(img_path,images,NEW_SIZE)#将图片改为统一尺寸
+    put_on_wall(images,keyword, WALL_SIZE, NEW_SIZE, row , column, x_offset, y_offset)#将重新裁剪后的图片放入图片墙
+
+#将图片改为统一尺寸
+def img_resize(img_path,images,NEW_SIZE):
+    img = Image.open(img_path)
+    r_img = img.resize(NEW_SIZE, Image.NEAREST)
+    images.append(r_img)
+
+#计算墙内每张图片的size
+def get_oneimg_size(WALL_SIZE,img_number):
+    one_area = (WALL_SIZE[0] * WALL_SIZE[1]) // (img_number)
+    a = int(math.sqrt(one_area))
+    row_x, colum_x = get_row_column(WALL_SIZE, (a,a)) #为确保都能放下，重新规划
+    one_area = (WALL_SIZE[0] * WALL_SIZE[1]) // ((row_x+1)*(colum_x+1))
+    a = int(math.sqrt(one_area))  # 宽、高相等
+    return (a,a)
+
+#计算行、列数
+def get_row_column(WALL_SIZE,NEW_SIZE):
+    row = WALL_SIZE[1] // NEW_SIZE[1]
+    column = WALL_SIZE[0] // NEW_SIZE[0]
+    return row, column
+
+#计算行列间隔值
+def get_offset(WALL_SIZE,NEW_SIZE,row,column):
+    y_offset = (WALL_SIZE[1] - row * NEW_SIZE[1]) // row
+    x_offset = (WALL_SIZE[0] - column * NEW_SIZE[0]) // row
+    return x_offset//2, y_offset//2   #为确保放的下
+
+#将重新裁剪后的图片放入图片墙
+def put_on_wall(images,keyword, WALL_SIZE, NEW_SIZE, row , column, x_offset, y_offset):
+    photo_wall_img = Image.new('RGB',WALL_SIZE,'white')
+    index = len(images)
+    for i in range (row):
+        for j in range(column):
+            if index > 0:
+                photo_wall_img.paste(images[len(images)-index],(j*NEW_SIZE[0]+(j+1)*x_offset, i*NEW_SIZE[1]+(i+1)*y_offset))
+                index-=1
+            else:
+                break
+    print("照片墙制作完成！\n共制作{}张图片".format(len(images)-index))
+    save_img(photo_wall_img,keyword)
+
+#将照片墙存入一个文件夹
+def save_img(photo_wall_img,keyword):
+    new_path_name = 'photowallimages'
+    wall_img_path = os.getcwd() + os.path.sep + new_path_name #新建的文件夹的路径
+    if not os.path.exists(wall_img_path):
+        os.makedirs(wall_img_path)
+    else:
+        print("照片墙文件夹已存在！")
+    photo_wall_img.save(new_path_name+ os.path.sep + keyword + '.jpg')
+
+
+def main(keyword):
+    images = []  # 存储要放入照片墙的、重新统一过尺寸的图片
+    WALL_SIZE = (1920, 1080)  # 照片墙尺寸
+    source_path = os.getcwd() + os.path.sep + keyword
+    if os.path.exists(source_path):
+        get_images(images, source_path, keyword, WALL_SIZE)  # 连续获取文件夹的图片，并制成照片墙
+    else:
+        print("未找到图片文件夹")
+
+
+if __name__ == "__main__":
+    keyword = '图片'
+    main(keyword)
+
